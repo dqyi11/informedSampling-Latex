db653aea530d3d4cffa3ce2ece157bb2ae110b2b
Daqing Yi <dqyi11@gmail.com>
2017-09-12 11:12:22 -0700

    new figures

--- code/sample efficiency/2d/plot1.m
+++ code/sample efficiency/2d/plot1.m
@@ -1,9 +1,9 @@
 data = sortrows(dim2, 5);
 
-data(:,1) = log(data(:,1));
-data(:,2) = log(data(:,2));
-data(:,3) = log(data(:,3));
-data(:,4) = log(data(:,4));
+%data(:,1) = log(data(:,1));
+%data(:,2) = log(data(:,2));
+%data(:,3) = log(data(:,3));
+%data(:,4) = log(data(:,4));
 
 green = [49,163,84]/255;
 red = [222 45 38]/255;
@@ -15,13 +15,13 @@ pink = [197,27,138]/255;
 figure;
 hold on;
 marker_size = 20;
-scatter(log(data(:,5)),data(:,4)./data(:,6), marker_size, 'MarkerFaceColor', blue, 'MarkerEdgeColor', blue, 'Marker', 's');
-scatter(log(data(:,5)),data(:,2)./data(:,6), marker_size, 'MarkerFaceColor', red, 'MarkerEdgeColor', red, 'Marker', 'd');
-scatter(log(data(:,5)),data(:,1)./data(:,6), marker_size, 'MarkerFaceColor', green, 'MarkerEdgeColor', green, 'Marker', 'h');
-scatter(log(data(:,5)),data(:,3)./data(:,6), marker_size, 'MarkerFaceColor', purple, 'MarkerEdgeColor', purple, 'Marker', 'o');
+scatter(log10(data(:,5)), log10(data(:,4)./data(:,6)), marker_size, 'MarkerFaceColor', blue, 'MarkerEdgeColor', blue, 'Marker', 's');
+scatter(log10(data(:,5)),log10(data(:,2)./data(:,6)), marker_size, 'MarkerFaceColor', red, 'MarkerEdgeColor', red, 'Marker', 'd');
+scatter(log10(data(:,5)),log10(data(:,1)./data(:,6)), marker_size, 'MarkerFaceColor', green, 'MarkerEdgeColor', green, 'Marker', 'h');
+scatter(log10(data(:,5)),log10(data(:,3)./data(:,6)), marker_size, 'MarkerFaceColor', purple, 'MarkerEdgeColor', purple, 'Marker', 'o');
 
-xlabel('informed set volume ratio');
-ylabel('time per sample (ms)');
+xlabel('informed set volume ratio - log_1_0');
+ylabel('time per sample - log_1_0(ms)');
 legend('Rejection', 'HRS', 'MCMC', 'Hit&Run');
 hold off;
 
